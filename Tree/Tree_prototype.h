fb3ee21917e4e88a9ee7ecfe2a020af54f77defa
mohamedkamalyacine <mohamedkamalyacine@gmail.com>
2022-12-24 17:34:47 +0200

    GetMaxDepth was added

--- Tree/Tree_prototype.h
+++ Tree/Tree_prototype.h
@@ -5,9 +5,23 @@ typedef struct Node Node;
 
 struct Node
 {
-    int data;
-    Node* leftChild;
-    Node* rightChild;
+    int Data;
+    Node *Left, *Right;
 };
+
+typedef struct Tree
+{
+    Node *Root;
+
+}Tree;
+
+void Add(Tree *tree, int data);
+Node* GetNodeByData(Tree *tree, int data);
+Node* GetParentNode(Tree *tree, Node *node);
+Node* GetMaxRight(Node *node);
+Node* GetMaxLeft(Node *node);
+void Remove(Tree *tree, int data);
+void Display(Node *node);
+int GetMaxDepth(Tree *tree);
 
 #endif // TREE_PROTOTYPE_H_INCLUDED
--- Tree/main.c
+++ Tree/main.c
@@ -4,7 +4,16 @@
 
 int main()
 {
-    Node n1;
-    //printf("Hello world!\n");
+    Tree T1 = {.Root = NULL};
+    Add(&T1, 50);
+    Add(&T1, 60);
+    Add(&T1, 70);
+    Add(&T1, 30);
+    Add(&T1, 20);
+    Add(&T1, 10);
+    printf("Root = %d\n", T1.Root->Data);
+    printf("Max Right = %d\n", (GetMaxRight(T1.Root))->Data);
+    printf("Max Left  = %d\n", (GetMaxLeft(T1.Root))->Data);
+    printf("Max Depth  = %d\n", GetMaxDepth(&T1));
     return 0;
 }
